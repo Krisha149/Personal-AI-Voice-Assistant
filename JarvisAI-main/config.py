f0ecb914cd08f515a6ecd8f05dd84cb19d46eed0
Krisha149 <117824191+Krisha149@users.noreply.github.com>
2026-08-21 16:31:03 +0530

    Update config.py

--- JarvisAI-main/config.py
+++ JarvisAI-main/config.py
@@ -1,13 +1,2 @@
 
-
-# apikey="AIzaSyCCNCvpV6J3OPN92ZjItgMN6D3bJ15CS-M"
-# apikey="AIzaSyDJaAm57OYuxoj4nBRtqJZmflkFkdriEtA"
-
-# apikey = "AIzaSyCtlGY_Wbovqdd5ToQkgoInjm9t_S75zV8"
-apikey="AIzaSyBHAKdN-pKZMoXLDHkewuEMhNCa3DifVuc"
-
-
-
-# //extraa:
-# AIzaSyBHAKdN-pKZMoXLDHkewuEMhNCa3DifVuc
-# AIzaSyC2Ao7PzhWZLJc7K4EV5kRahamULDRoCfE
+apikey="enter your api key"
